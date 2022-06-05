8e61434d02ccbc34b3d679c6b91042429b07fe46
Ян <Shatoshi@mail.ru>
2022-06-05 22:41:01 +0300

    ДЗ

---
+++ src/test/java/pro/sky/collectionsandsets/CollectionsandsetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionsandsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsandsetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
